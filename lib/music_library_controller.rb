f74f5aae264a42a07d512a764c8c39996cfadf9f
Carden Z <ephemere@protonmail.com>
2019-04-18 21:00:39 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -42,10 +42,10 @@ class MusicLibraryController
           list_songs_by_genre
         when "play song"
           play_song
-        # else
-        #   puts "Invalid entry. Please select from the following menu:"
-        #   menu
-        #   input = gets.chomp
+        else
+          puts "Invalid entry. Please select from the following menu:"
+          menu
+          input = gets.chomp
         end
     end
   end
