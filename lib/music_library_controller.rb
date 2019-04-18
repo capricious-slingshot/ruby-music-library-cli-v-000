999c8d111473eeb73a9f50118b4a7c38a1dd2bc7
Carden Z <ephemere@protonmail.com>
2019-04-18 20:58:44 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -42,7 +42,7 @@ class MusicLibraryController
         when "list genres"
           list_genres
         when "list artist"
-          list_artist
+          list_songs_by_artist
         when "list genre"
           list_genre
         when "play song"
